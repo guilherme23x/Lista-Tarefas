ecd4dc027bdd78cd01e5abda5e86b05278d48efb
Guilherme <150593535+guilherme23x@users.noreply.github.com>
2025-01-20 16:34:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const botao = document.querySelector(".botao_adicionar");
 const input = document.querySelector(".digitar");
 const listaCompleta = document.querySelector(".lista_tarefas");
+const modal = document.getElementById("modal");
+const botaoFecharModal = document.getElementById("fecharModal");
 let minhaLista = [];
 
 function adicionarNovaTarefa() {
+  if (input.value.trim() === "") {
+    mostrarModal(); // Mostra o modal se o campo estiver vazio
+    return; // Interrompe a execução da função
+  }
+
   minhaLista.push({
     tarefa: input.value,
     concluida: false,
@@ -38,6 +45,7 @@ function mostrarTarefas() {
       novaLi +
       `
     <li class="tarefa ${item.concluida && "done"}">
+    
         <img  src="./img/checked.png" alt="check-tarefa" onclick="concluirTarefa(${posicao})"/>
         <p>${item.tarefa}</p>
         <img  src="./img/trash.png" alt="Lixeira" onclick="deletar(${posicao})" />
@@ -48,6 +56,14 @@ function mostrarTarefas() {
 
   localStorage.setItem("lista", JSON.stringify(minhaLista));
 }
+
+function mostrarModal() {
+  modal.style.display = "flex"; // Exibe o modal
+}
+
+botaoFecharModal.addEventListener("click", function () {
+  modal.style.display = "none"; // Fecha o modal
+});
 
 recarregarTarefas();
 botao.addEventListener("click", adicionarNovaTarefa);
